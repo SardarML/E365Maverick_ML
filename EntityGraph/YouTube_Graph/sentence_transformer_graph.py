8a4dc015bb714595efecadc47237253fe5656006
Sardar <sara.sardar@bechtle.com>
2023-05-01 18:25:25 +0200

    added entities

--- EntityGraph/YouTube_Graph/sentence_transformer_graph.py
+++ EntityGraph/YouTube_Graph/sentence_transformer_graph.py
@@ -6,58 +6,57 @@ from get_merged_data import get_data
 
 merged_df, str_lst, vocab, identifier_vocab, identifier = get_data('graph_nodes/youtube_video_data/nodes/merged_data_for_AI.csv')
 
-# # get embeddings like in doc2vec
-# def SentenceTransformer_embeddings():
-#         '''https://huggingface.co/sentence-transformers/paraphrase-multilingual-mpnet-base-v2
-#         This is a sentence-transformers model: 
-#         It maps sentences & paragraphs to a 768 
-#         dimensional dense vector space and can 
-#         be used for tasks like clustering or semantic search.'''
+# get embeddings like in doc2vec
+def SentenceTransformer_embeddings():
+        '''https://huggingface.co/sentence-transformers/paraphrase-multilingual-mpnet-base-v2
+        This is a sentence-transformers model: 
+        It maps sentences & paragraphs to a 768 
+        dimensional dense vector space and can 
+        be used for tasks like clustering or semantic search.'''
 
-#         model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
-#         embeddings = model.encode(str_lst)
-#         embeddings = pd.DataFrame(embeddings)
-#         embeddings.to_csv('data/sentence_transformer_embedding.csv')
-#         embeddings = np.array(embeddings)
-#         print(embeddings.shape)
-#         return model, embeddings
+        model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+        embeddings = model.encode(str_lst)
+        embeddings = pd.DataFrame(embeddings)
+        embeddings.to_csv('data/sentence_transformer_embedding.csv')
+        embeddings = np.array(embeddings)
+        print(embeddings.shape)
+        return model, embeddings
 
-# import numpy as np
-# from rdflib import Graph, Literal, RDF, URIRef, BNode, SDO
-# from rdflib.namespace import RDF
+import numpy as np
+from rdflib import Graph, Literal, RDF, URIRef, BNode, SDO
+from rdflib.namespace import RDF
 
-# model, embeddings = SentenceTransformer_embeddings()
+model, embeddings = SentenceTransformer_embeddings()
 
 
-# def fill_graph_sentence_transformer(query):
-#     # graph initialization
-#     g = Graph()
-#     # encode query
-#     query_vec = np.array(model.encode([query])[0]).reshape(1,-1)
+def fill_graph_sentence_transformer(query):
+    # graph initialization
+    g = Graph()
+    # encode query
+    query_vec = np.array(model.encode([query])[0]).reshape(1,-1)
 
-#     # loop for calculating similarities and saving them in the list
-#     similarity_list = []
-#     for sent in vocab:
-#           sent_vec = np.array(model.encode([sent])[0]).reshape(1,-1)
-#           sim = cosine_similarity(query_vec, sent_vec)[0][0]
-#           similarity_list.append((sent, sim))
+    # loop for calculating similarities and saving them in the list
+    similarity_list = []
+    for sent in vocab:
+          sent_vec = np.array(model.encode([sent])[0]).reshape(1,-1)
+          sim = cosine_similarity(query_vec, sent_vec)[0][0]
+          similarity_list.append((sent, sim))
 
-#           # sorting the list of similarities in descending order by the 2nd element (similarity)
-#           sorted_list = sorted(similarity_list, key=lambda x: x[1], reverse=True)
+          # sorting the list of similarities in descending order by the 2nd element (similarity)
+          sorted_list = sorted(similarity_list, key=lambda x: x[1], reverse=True)
 
-#     # define nodes
-#     query_term = BNode()
-#     g.add((query_term, RDF.type, SDO.DefinedTerm))
-#     g.add((query_term, SDO.termCode, Literal(query)))
+    # define nodes
+    query_term = BNode()
+    g.add((query_term, RDF.type, SDO.DefinedTerm))
+    g.add((query_term, SDO.termCode, Literal(query)))
 
-#     # parsing output of the sorted list in RDF
-#     for item in sorted_list[:5]:
-#         id = identifier_vocab.get(item[0])  # retrieve identifier for the current element
-#         learning_resource = BNode()
-#         g.add((learning_resource, RDF.type, SDO.LearningResource))
-#         g.add((learning_resource, SDO.identifier, Literal(id)))
-#         g.add((learning_resource, SDO.title, Literal(f'Entity: {item[0]} ; cosine similarity to the refered skill: {query}: {item[1]}')))
-#         g.add((learning_resource, SDO.teaches, query_term))
-
-#     return g
+    # parsing output of the sorted list in RDF
+    for item in sorted_list[:5]:
+        id = identifier_vocab.get(item[0])  # retrieve identifier for the current element
+        learning_resource = BNode()
+        g.add((learning_resource, RDF.type, SDO.LearningResource))
+        g.add((learning_resource, SDO.identifier, Literal(id)))
+        g.add((learning_resource, SDO.title, Literal(f'Entity: {item[0]} ; cosine similarity to the refered skill: {query}: {item[1]}')))
+        g.add((learning_resource, SDO.teaches, query_term))
+    return g
         
--- EntityGraph/YouTube_Graph/txtai_skill_classification_graph.py
+++ EntityGraph/YouTube_Graph/txtai_skill_classification_graph.py
@@ -6,53 +6,53 @@ from get_merged_data import get_data
 merged_df, str_lst, vocab, identifier_vocab, identifier = get_data('graph_nodes/youtube_video_data/nodes/merged_data_for_AI.csv')
 strings = merged_df['String']
 
-# # work with the embeddings class
-# embeddings = Embeddings({'path':'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'})
+# work with the embeddings class
+embeddings = Embeddings({'path':'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'})
 
-# txtai_data = []
-# i=0
-# for id, text in zip(identifier, strings):
-#     ''' appending the index, the text 
-#     and None because the embeddings object 
-#     is expecting 3 different components'''
-#     txtai_data.append((i, text, id))
-#     i+=1
+txtai_data = []
+i=0
+for id, text in zip(identifier, strings):
+    ''' appending the index, the text 
+    and None because the embeddings object 
+    is expecting 3 different components'''
+    txtai_data.append((i, text, id))
+    i+=1
 
-# print(f'we work with {len(txtai_data)} entitiy-textdata')
+print(f'we work with {len(txtai_data)} entitiy-textdata')
 
-# # let's quickly create our index of all the data
-# embeddings.index(txtai_data)
+# let's quickly create our index of all the data
+embeddings.index(txtai_data)
 
-# # save the embeddings index
-# embeddings.save('data/txtai_embeddings_encoded.csv')
+# save the embeddings index
+embeddings.save('data/txtai_embeddings_encoded.csv')
 
-# import numpy as np
-# from rdflib import Graph, Literal, URIRef, BNode, SDO
-# from rdflib.namespace import RDF
+import numpy as np
+from rdflib import Graph, Literal, URIRef, BNode, SDO
+from rdflib.namespace import RDF
 
-# def fill_graph_txtai(query):
-#         # graph initialization
-#         h = Graph()
+def fill_graph_txtai(query):
+        # graph initialization
+        h = Graph()
 
-#         # search for things that have the word __ in it and pass in how many results we want to see
-#         res = embeddings.search(query, 5)
+        # search for things that have the word __ in it and pass in how many results we want to see
+        res = embeddings.search(query, 5)
 
-#         for r in res:
-#             # retrieve the key from the tuple
-#             key = txtai_data[r[0]][2]
-#             # retrieve the text from the tuple
-#             text = txtai_data[r[0]][1]
+        for r in res:
+            # retrieve the key from the tuple
+            key = txtai_data[r[0]][2]
+            # retrieve the text from the tuple
+            text = txtai_data[r[0]][1]
             
-#             # define nodes
-#             query_term = BNode()
-#             h.add((query_term, RDF.type, SDO.DefinedTerm))
-#             h.add((query_term, SDO.termCode, Literal(query)))
+            # define nodes
+            query_term = BNode()
+            h.add((query_term, RDF.type, SDO.DefinedTerm))
+            h.add((query_term, SDO.termCode, Literal(query)))
 
 
-#             learning_resource = BNode()
-#             h.add((learning_resource, RDF.type, SDO.LearningResource))
-#             h.add((learning_resource, SDO.identifier, Literal(key)))
-#             # print the key, first 50 characters of the text and the similarity score
-#             h.add((learning_resource, SDO.title, Literal(f'Entity: {text[:50]} ; cosine similarity to the refered skill: {query}: {r[1]}')))
-#             h.add((learning_resource, SDO.teaches, query_term))    
-#         return h
+            learning_resource = BNode()
+            h.add((learning_resource, RDF.type, SDO.LearningResource))
+            h.add((learning_resource, SDO.identifier, Literal(key)))
+            # print the key, first 50 characters of the text and the similarity score
+            h.add((learning_resource, SDO.title, Literal(f'Entity: {text[:50]} ; cosine similarity to the refered skill: {query}: {r[1]}')))
+            h.add((learning_resource, SDO.teaches, query_term))    
+        return h
