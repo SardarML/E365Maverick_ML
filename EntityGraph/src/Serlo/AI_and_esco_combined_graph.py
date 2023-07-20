c23cc6d739994a618e61c47bce33073108338d34
Sardar <sara.sardar@bechtle.com>
2023-07-20 12:47:08 +0200

    serlo sentence transformer esco skill graph generated

--- EntityGraph/src/Serlo/AI_and_esco_combined_graph.py
+++ EntityGraph/src/Serlo/AI_and_esco_combined_graph.py
@@ -1,11 +1,10 @@
 from rdflib import Literal, Graph
 from serlo_sentence_transformer_graph import fill_graph_sentence_transformer
-from esco_graph import Esco_graph
 
 # esco graphs
-skill_graph = Esco_graph('data\Esco\skills_de.csv')
+skill_graph = Graph()
+skill_graph.parse('data/Esco/esco_skill_graph.ttl', format='ttl')
 print(f'skill graph has {len(skill_graph)} facts')
-#skill_graph.serialize(destination='data/Esco/esco_skill_graph.ttl')
 
 # Now lets take the objects of esco graphs as prompts for sentence transformer
 def fill_all(graph, transformer_function):
